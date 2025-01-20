fe6389346ab54b5a757d885f45fd1747980b7552
Yugdeep Parihar <yugdeepparihar098@gmail.com>
2025-01-20 15:07:16 +0530

    operators understanding done

---
+++ operators.cs
@@ -0,0 +1,14 @@
+using System;
+
+class operators {
+static void Main(string[] args) {
+int a=Convert.ToInt32(Console.ReadLine());
+int b =Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Addition: " + (a + b)); 
+Console.WriteLine("Subtraction: " + (a - b)); 
+Console.WriteLine("Multiplication: " + (a * b)); 
+Console.WriteLine("Division: " + (a / b)); 
+Console.WriteLine("Modulus: " + (a % b)); 
+}
+}
