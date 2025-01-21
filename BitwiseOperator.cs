264368638f00400df7ffa2e4d5686c895dbb4283
Yugdeep Parihar <yugdeepparihar098@gmail.com>
2025-01-21 14:16:14 +0530

    bitwise operator completed

---
+++ BitwiseOperator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class TernaryOperator {
+static void Main(string[] args) {
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int max = (a > b) ? a : b;
+
+Console.WriteLine("Max: " + max);
+
+}
+}
