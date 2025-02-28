62f3b7369ae6176026cd6957711d4125da82b6a2
Yugdeep Parihar <yugdeepparihar098@gmail.com>
2025-02-28 16:36:19 +0530

    [Yugdeep Parihar] ADD: User Registration using ASP.NET core web api Submitted

---
+++ UserRegistration(Main)/BusinessLayer/Services/UserRegistrationBL.cs
@@ -0,0 +1,33 @@
+﻿using RepositoryLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Services;
+using RepositoryLayer.Services;
+namespace BusinessLayer.Services
+{
+    public class UserRegistrationBL
+    {
+        UserRegistrationRL _userRegistrationRL;
+
+
+        public UserRegistrationBL(UserRegistrationRL userRegistrationRL) { 
+           _userRegistrationRL =userRegistrationRL;
+        }
+        public string registrationBL(string username,string password)
+        {
+            var (user,pass) = _userRegistrationRL.registrationRL();
+
+            if(user==username && pass == password)
+            {
+                return "Registration Successful";
+            }
+            else
+            {
+                return "Registration Failed";
+            }   
+        }
+    }
+}
---
+++ UserRegistration(Main)/UserRegistration/Controllers/UserRegistrationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BusinessLayer.Services;
+
+namespace UserRegistration.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserRegistrationController : ControllerBase
+    {
+        UserRegistrationBL _userRegistrationBL;
+        public UserRegistrationController(UserRegistrationBL userRegistrationBL)
+        {
+            _userRegistrationBL = userRegistrationBL;
+        }
+
+        [HttpGet]
+        public string registration()
+        {
+            string username = "root";
+            string password = "root";
+            return _userRegistrationBL.registrationBL(username,password);
+        }
+    }
+}
