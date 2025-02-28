8dd1b244945f12e083d52223527841aea65054e2
Yugdeep Parihar <yugdeepparihar098@gmail.com>
2025-02-28 22:45:23 +0530

    [Yugdeep Parihar] ADD: User Registration Web app with storage of registered and create option and checked via postman api test done

--- UserRegistration(Main)/UserRegistration/obj/Debug/net6.0/UserRegistration.AssemblyInfo.cs
+++ UserRegistration(Main)/UserRegistration/obj/Debug/net6.0/UserRegistration.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserRegistration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62f3b7369ae6176026cd6957711d4125da82b6a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserRegistration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserRegistration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ UserRegistrationWebApp(List Storage)/ModelLayer/DTO/ResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ModelLayer.DTO
+{
+    public class ResponseModel<T>
+    {
+        public string success { get; set; } = "true";
+        public string message { get; set; } = "";
+        public T? data { get; set; }
+    }
+}
